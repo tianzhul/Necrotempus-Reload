5dc47c10d329ee8d019fee8f19e7e622524eaa46
tianzhul <142957030+tianzhul@users.noreply.github.com>
2025-03-21 23:07:27 +0800

    Update GlyphConfigPacketHandler.java

--- src/main/java/io/github/cruciblemc/necrotempus/packet/GlyphConfigPacketHandler.java
+++ src/main/java/io/github/cruciblemc/necrotempus/packet/GlyphConfigPacketHandler.java
@@ -4,15 +4,16 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.github.cruciblemc.necrotempus.NecroTempus;
-import io.github.cruciblemc.necrotempus.packet.GlyphConfigPacket;
+import net.minecraft.client.Minecraft;
 
 public class GlyphConfigPacketHandler implements IMessageHandler<GlyphConfigPacket, IMessage> {
     @Override
     public IMessage onMessage(GlyphConfigPacket message, MessageContext ctx) {
-        if (ctx.side.isClient()) {
-            NecroTempus.getInstance().getGlyphManager()
-                    .handleConfigUpdate(message.getConfigData());
-        }
+        // 使用 1.7.10 兼容的主线程调度
+        Minecraft.getMinecraft().func_152344_a(() -> {
+            NecroTempus.getInstance().getLogger().info("收到配置数据: " + message.getConfigData());
+            NecroTempus.getInstance().getGlyphManager().handleConfigUpdate(message.getConfigData());
+        });
         return null;
     }
 }
